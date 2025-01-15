69dfdf168b744bfaf90ce4b1b766c347d6357147
Cristian Scarella <cristianscarella09@hotmail.com>
2025-01-14 21:40:47 -0300

    express-rate-limit package added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import { rateLimit } from 'express-rate-limit'
 import { routerWeather } from './routes/weather.js'
 import errorHandler from './middlewares/errorHandler.js'
 
 export const app = express()
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+})
+app.use(limiter)
+
 app.use('/weather', routerWeather)
 
 app.disable('x-powered-by')
+
 app.use(errorHandler)
